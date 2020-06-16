48bdf22007c43deeb8ac743a7cc06d23a7f874d6
Keegan Naidoo <keegann51@gmail.com>
2020-06-16 10:51:51 +0200

    Added a Person Bio App using Relative Layout

--- app/src/main/java/com/example/myapplication/ListViewActivity.java
+++ app/src/main/java/com/example/myapplication/ListViewActivity.java
@@ -1,10 +1,10 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,14 +12,12 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ListViewActivity extends AppCompatActivity {
 
     private android.widget.ListView listView;
-    private String [] options = {
+    private String[] options = {
 
-            "A",
-            "B",
+            "Introduction",
+            "My Bio App - Relative Layout",
             "C",
             "D"
-
-
     };
 
     @Override
@@ -27,14 +25,26 @@ public class ListViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listview);
 
-        listView= findViewById(R.id.listview);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,options);
+        listView = findViewById(R.id.listview);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, options);
         listView.setAdapter(arrayAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(ListViewActivity.this, options[i], Toast.LENGTH_SHORT).show();
+                Intent intent;
+                switch (i) {
+                    case 0:
+
+                        intent = new Intent(ListViewActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        break;
+
+                    case 1:
+                        intent = new Intent(ListViewActivity.this, MyBioRealtiveLayout.class);
+                        startActivity(intent);
+                        break;
+                }
             }
         });
     }
